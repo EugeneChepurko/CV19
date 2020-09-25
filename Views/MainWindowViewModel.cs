028f798ff346123655562d638d9bce50edd8560f
Eugene <chepurko0@gmail.com>
2020-09-26 00:18:49 +0300

    Added command to close the application

--- Views/MainWindowViewModel.cs
+++ Views/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CV19.Views.Base;
+﻿using CV19.Infrastructure.Commands;
+using CV19.Views.Base;
+using System.Windows;
+using System.Windows.Input;
 
 namespace CV19.Views
 {
@@ -15,6 +18,24 @@ namespace CV19.Views
         {
             get => status;
             set { OnPropertyChanged(ref status, value); }
+        }
+
+        #region Commands
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
         }
     }
 }
