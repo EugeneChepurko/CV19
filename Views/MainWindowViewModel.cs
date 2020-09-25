d83c19408e1575197323c772b048a1c8f6a0208f
Eugene <chepurko0@gmail.com>
2020-09-25 22:09:15 +0300

    Base infrastructure

---
+++ Views/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CV19.Views.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Views
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+    }
+}
