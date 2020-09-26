b99e00891518167be7f46437d8ab01cffb173df6
Eugene <chepurko0@gmail.com>
2020-09-26 11:31:47 +0300

    logic of creates a points at diagram

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,12 +14,63 @@ namespace CV19Console
     {
         private const string urlData = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+        private static async Task<Stream> GetDataStream()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(urlData, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        public static IEnumerable<string> GetDataLines()
+        {
+            using (var dataStream = GetDataStream().Result)
+            {
+                using (var streamReader = new StreamReader(dataStream))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        var line = streamReader.ReadLine();
+                        if (string.IsNullOrWhiteSpace(line)) continue;
+                        yield return line;
+                    }
+                }
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(d => DateTime.Parse(d, CultureInfo.InvariantCulture))
+            .ToArray();
+
+        private static IEnumerable<(string country, string province, int[] count)>GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(','));
+
+            foreach (var row in lines)
+            {
+                var province = row[0].Trim(',');
+                var country = row[1].Trim(' ', '"');
+                var count = row.Skip(4).Select(int.Parse).ToArray();
+
+                yield return (country, province, count);
+            } 
+        }
+
         static void Main(string[] args)
         {
-            var client = new HttpClient();
+            //var client = new HttpClient();
 
-            var response = client.GetAsync(urlData).Result;
-            var csv_str = response.Content.ReadAsStringAsync().Result;
+            //var response = client.GetAsync(urlData).Result;
+            //var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            foreach (var item in GetDataLines())
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,10 @@
 ﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,6 +12,13 @@ namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+
+        private IEnumerable<DataPoint> testDataPoints;
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => testDataPoints;
+            set { OnPropertyChanged(ref testDataPoints, value); }
+        }
         /// <summary>
         /// App status
         /// </summary>
@@ -36,6 +48,17 @@ namespace CV19.ViewModels
         public MainWindowViewModel()
         {
             CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            var dataPoints = new List<DataPoint>((int)(360 / 0.1));
+            for (double x = 0d; x <= 360; x+=0.1)
+            {
+                const double toRad = Math.PI / 180;
+                var y = Math.Sin(x * toRad);
+
+                dataPoints.Add(new DataPoint { XValue = x, YValue = y });
+            }
+
+            TestDataPoints = dataPoints;
         }
     }
 }
